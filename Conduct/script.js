20287f3bdb801e5a637fd7c09ea84d49d694c976
roy-lau <roy-lau@foxmail.com>
2017-06-26 09:47:22 +0800

    增加接口的回调函数

--- Conduct/script.js
+++ Conduct/script.js
@@ -1,35 +1,36 @@
-;(function($) {
+;
+(function($) {
     "use scrict"
     return OIO = {
         // 验证传来的数据
-        Verification: function(parm, title) {
+        Verification: function(parm, title,callback) {
             // 判断参数(parm)为是否空
             if (parm && parm !== '') {
-                this.extend(parm, title)
+                this.extend(parm, title,callback)
             } else {
                 // 如果没有参数，则返回null
                 return null;
             }
         },
         // 揉入一些默认参数(减少外部传入的参数冗余)
-        extend: function(parm, title){
-        	var Default = {
+        extend: function(parm, title,callback) {
+            var Default = {
                 "rsvd": "0",
                 "ver": "1.0",
-                };
-        	var json = $.extend(Default,parm);
-        	this.LetterSort(json,title); // 调用排序
+            };
+            var json = $.extend(Default, parm);
+            this.LetterSort(json, title,callback); // 调用排序
         },
         // 字母排序
-        LetterSort: function(oldJson, title) {
+        LetterSort: function(oldJson, title,callback) {
             var newJson = {};
             Object.keys(oldJson).sort().forEach(function(item) {
                 newJson[item] = oldJson[item]
             })
-            this.autograph(newJson, title) // 调用签名
+            this.autograph(newJson, title,callback) // 调用签名
         },
         // 签名
-        autograph: function(sortJson, title) {
+        autograph: function(sortJson, title,callback) {
             /*
             签名算法如下：
                 1、将json的key值按字母排序
@@ -42,18 +43,19 @@
             */
             var autograph = md5(title + JSON.stringify(sortJson)
                     .replace(/\:|\,|\"|\'|\{|\}/g, ""))
-                    .toUpperCase()
-                    .toString(16)
-            this.AddSid(autograph, sortJson)
+                .toUpperCase()
+                .toString(16)
+            this.AddSid(autograph, sortJson,callback)
         },
         // 添加签名(sid)
-        AddSid: function(autograph, json) {
+        AddSid: function(autograph, json,callback) {
             json.sid = autograph;
             // 传给post
-            this.post(json);
+            this.post(json,callback);
         },
         // 发送数据
-        post: function(json) {
+        post: function(json,callback) {
+            var self = this;
             console.log(json)
             $.ajax({
                 url: "http://101.201.101.70:5000/",
@@ -63,46 +65,29 @@
                 contentType: "application/json",
                 success: function(res, data) {
                     alert("success")
-                    console.log(res, data)
+                    callback(res, data)
                 },
-                error: function(res, textStatus, errorThrown) {
-                    console.log(res.status);
-                    console.log(res.readyState);
-                    console.log(textStatus);
+                error: function(res, data) {
+                    callback(res,data);
                 }
             })
         },
-        // axyb模式执行此方法
-        axyb: function(sid) {
-
-        },
-        // 语音验证业务
-        voice: function(sid) {
-
-        },
-        // ax绑定
-        axBind: function(json) {
-            var title = "L9HASRNCM0IQ" // 头部信息
-            json.service="acbss",
-            this.Verification(json, title) // 验证传来的数据
-        },
-         // ax解绑
-        axUnBind: function(json) {
-            var title = "L9HASRNCM0IQ" // 头部信息
-            json.service="acbss",
-            this.Verification(json, title) // 验证传来的数据
-        },
-         // ax查询
-        axQuery: function(json) {
-            var title = "L9HASRNCM0IQ" // 头部信息
-            json.service="acbss",
-            this.Verification(json, title) // 验证传来的数据
-        },
-        // ax更换真实号
-        axCheange: function(json) {
-            var title = "L9HASRNCM0IQ" // 头部信息
-            json.service="acbss",
-            this.Verification(json, title) // 验证传来的数据
+        interface: function(json, callback) {
+            var self = this;
+            switch (json.service) {
+                case "acbss":
+                    var title = "L9HASRNCM0IQ" // ax头部信息
+                    self.Verification(json, title, callback) // 验证传来的数据
+                    break;
+                case "acbs":
+                    var title = "L9HASRNCM0IQ" // 头部信息
+                    self.Verification(json, title, callback) // 验证传来的数据
+                    break;
+                case "acbs":
+                    var title = "L9HASRNCM0IQ" // 头部信息
+                    self.Verification(json, title, callback) // 验证传来的数据
+                    break;
+            }
         },
         // 获取时间戳
         getTs: function(offset) {
